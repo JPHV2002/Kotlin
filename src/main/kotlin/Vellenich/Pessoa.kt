00cc24e900733e7599d014a583ee4d051521310c
João Pedro Hegedus Vellenich <jpvellenich@gmail.com>
2021-03-03 16:33:22 -0300

    Private Set to age value and create a internal class for the address

--- src/main/kotlin/Vellenich/Pessoa.kt
+++ src/main/kotlin/Vellenich/Pessoa.kt
@@ -3,7 +3,10 @@ package Vellenich
 class Pessoa {
 
     var nome = "João"
+        private set
+
     var idade = 18
+        private set
 
     inner class Endereco{
         var rua = "rua"
@@ -15,5 +18,9 @@ class Pessoa {
 fun main(){
     val joao = Pessoa()
 
+    println(joao.nome)
+
+    println(joao.idade)
+    println(joao.nome)
     println(joao.Endereco().numero)
 }
